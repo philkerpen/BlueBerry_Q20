e83bbc770608ceca5bb8d15abe29f27ac350595d
philkerpen <105007756+philkerpen@users.noreply.github.com>
2025-03-03 17:35:43 -0500

    Create custom_keycodes.h

---
+++ config/custom_keycodes.h
@@ -0,0 +1,7 @@
+#ifndef CUSTOM_KEYCODES_H
+#define CUSTOM_KEYCODES_H
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+
+#define KC_APPLICATION ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_APPLICATION)
+#endif
